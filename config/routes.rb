efe28e9c086cd1f71c7f3a4e13fa68b32343b5f9
syo-tokeshi <tokeshishou24@gmail.com>
2022-07-05 16:55:07 +0900

    fix ルーティングテーブル

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
 
   #説明ページがrootパス
-  root "homes#index"
+  root "homes#index"  
 
-  #独自に追加
-  resources :users, :only => [:index,:show]
-  get 'users/autocall/:id' => "users#autocall"
-  get 'users/stop_autocall/:id' => "users#stop_autocall"
-  
-  
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'   
@@ -20,5 +14,10 @@ Rails.application.routes.draw do
     get "login", :to => "users/sessions#new"
     get "logout", :to => "users/sessions#destroy"
   end
+
+  #独自に追加
+  resources :users, :only => [:index,:show]
+  get 'users/autocall/:id' => "users#autocall"
+  get 'users/stop_autocall/:id' => "users#stop_autocall"
 
 end
