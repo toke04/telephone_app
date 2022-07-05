5421fc2d5ab9d8691b0e2d34826bc4092db4c629
syo-tokeshi <tokeshishou24@gmail.com>
2022-07-05 16:38:48 +0900

    fix ルーティングテーブル

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,24 @@
 Rails.application.routes.draw do
 
-  root "homes#index" #説明ページがrootパス
+  #説明ページがrootパス
+  root "homes#index"
+
+  #独自に追加
+  resources :users, :only => [:index,:show]
+  get 'users/autocall/:id' => "users#autocall"
+  get 'users/stop_autocall/:id' => "users#stop_autocall"
+  
+  
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'   
   } 
 
-devise_scope :user do
-  get "user/:id", :to => "users/registrations#detail"
-  get "signup", :to => "users/registrations#new"
-  get "login", :to => "users/sessions#new"
-  get "logout", :to => "users/sessions#destroy"
-end
-  resources :users, :only => [:index,:show,:autocall]
-   get 'users/index'
-   get 'users/show'
-   get 'users/autocall/:id' => "users#autocall"
-   get 'users/stop_autocall/:id' => "users#stop_autocall"
-  # devise_for :users
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
 
-  ## 開発環境用letter_opener
-  # mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
