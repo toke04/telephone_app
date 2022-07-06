95016ab44e61eaa2863c612268c9393e05e25072
syo-tokeshi <tokeshishou24@gmail.com>
2022-07-06 13:35:24 +0900

    fix route

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
 
-  #説明ページがrootパス
-  root "homes#index"  
-
+  root "homes#index" #説明ページがrootパス
   devise_for :users, :controllers => {
   :registrations => 'users/registrations',
   :sessions => 'users/sessions'   
   } 
 
-  devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
-  end
-
-  #独自に追加
+devise_scope :user do
+  get "user/:id", :to => "users/registrations#detail"
+  get "signup", :to => "users/registrations#new"
+  get "login", :to => "users/sessions#new"
+  get "logout", :to => "users/sessions#destroy"
+end
   resources :users, :only => [:index,:show,:autocall]
-    get 'users/index'
-    get 'users/show'
-    get 'users/autocall/:id' => "users#autocall"
-    get 'users/stop_autocall/:id' => "users#stop_autocall"
+   get 'users/index'
+   get 'users/show'
+   get 'users/autocall/:id' => "users#autocall"
+   get 'users/stop_autocall/:id' => "users#stop_autocall"
+
 end
